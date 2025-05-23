f4db2b2763df3adbbe6fdc6e6aa55714210e7538
seba <seba@anod.xyz>
2025-05-23 17:49:19 +0200

    info json fix

--- psono/psono/settings.py
+++ psono/psono/settings.py
@@ -731,12 +731,12 @@ def generate_signature():
         'avatar_max_size_kb': AVATAR_MAX_SIZE_KB,
     }
 
-    info = json.dumps(info)
+    infostr = json.dumps(info)
 
     signing_box = nacl.signing.SigningKey(PRIVATE_KEY, encoder=nacl.encoding.HexEncoder)
     verify_key = signing_box.verify_key.encode(encoder=nacl.encoding.HexEncoder)
     # The first 128 chars (512 bits or 64 bytes) are the actual signature, the rest the binary encoded info
-    signature = binascii.hexlify(signing_box.sign(info.encode()))[:128]
+    signature = binascii.hexlify(signing_box.sign(infostr.encode()))[:128]
 
     return {
         'info': info,
